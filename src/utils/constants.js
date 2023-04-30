58a76929ec8a459a68c52f9e78fea0763035225f
Tope Adekanbi <tope.adekanbi@gmail.com>
2023-04-30 05:11:24 -0400

    remove custom info

--- src/utils/constants.js
+++ src/utils/constants.js
@@ -1,5 +1,5 @@
-export const AMAZON_BOOK_LINKS = 'https://www.amazon.com/s?i=stripbooks&rh=p_27%3ADr.+Clarissa+N.+Henry-Adekanbi&s=relevancerank&text=Dr.+Clarissa+N.+Henry-Adekanbi&ref=dp_byline_sr_book_1';
+export const AMAZON_BOOK_LINKS = '/#amazon_book_link';
 
-export const TWITTER = 'https://twitter.com/HenryandJeffers';
-export const INSTAGRAM = 'https://www.instagram.com/henryandjefferspublishing/';
-export const FACEBOOK = 'https://www.facebook.com/HenryandJeffersPublishing/';
+export const TWITTER = 'https://twitter.com/<username>';
+export const INSTAGRAM = 'https://www.instagram.com/<username>/';
+export const FACEBOOK = 'https://www.facebook.com/<username>/';
